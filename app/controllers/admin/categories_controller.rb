27cbef611a534e7f2ca0dbc6f244134b009fe274
Erin Toth <erintoth@gmail.com>
2019-02-21 01:38:08 +0000

    Create views for Admin category handling

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,6 @@
 class Admin::CategoriesController < ApplicationController
   def index
+    @categories = Category.order(id: :desc).all
   end
 
   def new
@@ -7,6 +8,21 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Product created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
   end
 
 end
