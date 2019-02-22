bda1336d5b458eda78508757eb662c521afed841
Erin Toth <erintoth@gmail.com>
2019-02-22 03:19:08 +0000

    Add command to send email after successful order

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,9 +1,9 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "no-reply@jungle.com"
 
-  def order_receipt(user)
-      @user = user
-      mail(to: @user.email, subject: "SOME SUBJECT!")
+  def order_receipt(order)
+      @order = order
+      mail(to: @order.email, subject: "Order Receipt")
   end
 
   layout 'mailer'
