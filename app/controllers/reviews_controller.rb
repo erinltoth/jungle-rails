0c84d018f4c3e398c0ec0cd950ef871895ed243d
Erin Toth <erintoth@gmail.com>
2019-02-23 00:39:01 +0000

    Add delete functionality for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,5 @@
 class ReviewsController < ApplicationController
+  before_action :require_login
   def create
     @review = Review.new(review_params)
     @product = Product.find params[:product_id]
@@ -11,6 +12,23 @@ class ReviewsController < ApplicationController
     else
       redirect_to [:products], notice: 'Shit is broken'
       puts "This didn't work"
+    end
+  end
+
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+
+    redirect_to controller: 'products', action: 'show', id: params[:product_id]
+  end
+
+  private
+
+  def require_login
+    unless current_user
+      flash[:error] = "You must be logged in to post reviews."
+      redirect_to controller: 'products', action: 'show', id: params[:product_id]
     end
   end
 
