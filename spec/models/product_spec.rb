f4efef087254944588d71fb42c4e66e83e0d0244
Erin Toth <erintoth@gmail.com>
2019-02-25 21:15:34 +0000

    Write inital product_spec tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' 
+  subject { described_class.new }
+  
+  it 'should not be valid if name is missing' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+    
+  it 'should not be valid if price is missing' do
+    subject.price_cents = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should not be valid if quantity is missing' do
+    subject.quantity = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should not be valid if category is missing' do
+    subject.category_id = nil
+    expect(subject).to_not be_valid
+  end
 end
